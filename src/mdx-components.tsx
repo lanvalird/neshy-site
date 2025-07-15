4703e5a57fd83cad766ba5f560561e10b8d0d548
lanvalird <strazhognay@gmail.com>
2025-07-15 20:30:04 +0400

    fix building (eslint...)

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -5,6 +5,7 @@ import type { MDXComponents } from 'mdx/types';
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
+    /* eslint-disable  @typescript-eslint/no-explicit-any */
     img: (props) => <ImageZoom className="rounded-lg" {...(props as any)} />,
     ...components,
   };
